e43814f1fa7e12e93268e5595401749f24bd4adb
BryanM-Dev <42093195+BryanM-Dev@users.noreply.github.com>
2018-08-04 12:38:31 -0400

    Added JavaApplication6
    JavaApplication6 is the Java Build and Java Program for this repo.

---
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+import java.util.Scanner;
+/**
+ *
+ * @author dapcep
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.println("test");
+    Scanner in = new Scanner(System.in);
+    String daddy = "Bryan";
+    String name = in.nextLine();
+    if (daddy.equals(name)) {
+        System.out.println("Hello d4ddy");
+    }
+    else {        
+    System.out.println("ayy " + name + ", do u play fortnut?");
+    }
+    
+}
+}
